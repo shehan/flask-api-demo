55c3efe84a4ced0043cb65d35c7e76b3b14887a2
shehan <shehan.peruma@gmail.com>
2021-09-23 23:07:15 -0400

    updated

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/F2C/<int:value>')
 def fahrenheit_to_celsius(value):
     result = str((value - 32) * 5 / 9)
-    return "%sF = %sC" % (value, result)
+    return jsonify(fahrenheit=value, celsius=result)
 
 
 @app.route('/C2F/<int:value>')
 def celsius_to_fahrenheit(value):
     result = str((value * 9 / 5) + 32)
-    return "%sC = %sF" % (value, result)
+    return jsonify(fahrenheit=result, celsius=value)
 
 
 if __name__ == "__main__":
